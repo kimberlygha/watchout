7da639fd495807362aac4a748716f9853d0ea259
kimberlygha <kimberlygha@gmail.com>
2015-10-16 16:49:26 -0700

    add collision detection

--- client/watchout.js
+++ client/watchout.js
@@ -1,3 +1,6 @@
+var curScore = 0;
+var highScore = 0;
+
 var svg = d3.select('body').append('svg').attr('height', '500').attr('width', '500');
 
 var makeEnemy = function(n){
@@ -6,7 +9,7 @@ var makeEnemy = function(n){
     .attr('r', '20').attr('cx', Math.random()*500).attr('cy', Math.random()*500)
     .style('fill', 'grey');
   }
-}
+};
 
 makeEnemy(10);
 
@@ -16,18 +19,29 @@ setInterval(function(){
   }).attr('cy', function(){
     return Math.random()*500;
   });
-}, 1500)
+}, 1500);
 
-var drag = function(){ 
-  d3.behavior.drag()
-  console.log("drag");
-}
-
-d3.select('rect').on('.drag', drag);
+var drag = d3.behavior.drag().on('drag', function(){
+  svg.select('rect').attr('x', d3.event.x).attr('y', d3.event.y);
+  svg.selectAll('circle').each(checkCollision);
+});
 
 var makeHero = function(){
   svg.append("rect").attr('x', 250).attr('y', 250).attr('width', 30).attr('height', 30).style('fill', 'red')
-  .attr('draggable', 'true');
-}
+  .attr('draggable', 'true').call(drag);
+};
 
 makeHero();
+
+var checkCollision = function(){
+  var radiusSum = 35;
+  var xDiff = parseInt(d3.select(this).attr('cx')) - parseInt(svg.select('rect').attr('x'));
+  var yDiff = parseInt(d3.select(this).attr('cy')) - parseInt(svg.select('rect').attr('y'));
+  var distance = Math.sqrt(xDiff*xDiff + yDiff*yDiff);
+  if (distance < radiusSum) {
+    curScore = 0;
+    console.log(curScore);
+  }
+};
+
+
